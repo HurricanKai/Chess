cd8eb5d30e7b475147f9b85b2d71f519fdb90706
Nils <imrebux@gmail.com>
2021-09-21 10:05:24 +0200

    :tada: Project setup

---
+++ src/de/nosswald/chess/Chess.java
@@ -0,0 +1,7 @@
+package de.nosswald.chess;
+
+public class Chess
+{
+    public static final String APP_NAME = "Chess";
+    public static final String APP_VERSION = "0.1";
+}
---
+++ src/de/nosswald/chess/Start.java
@@ -0,0 +1,9 @@
+package de.nosswald.chess;
+
+public class Start
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
