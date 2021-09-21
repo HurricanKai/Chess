3def021adf5a0a1a88d63d0294d45f86124fd34d
Nils <imrebux@gmail.com>
2021-09-21 12:00:56 +0200

    :sparkles: Added pieces

--- src/de/nosswald/chess/game/Board.java
+++ src/de/nosswald/chess/game/Board.java
@@ -1,10 +1,41 @@
 package de.nosswald.chess.game;
 
 import de.nosswald.chess.game.piece.Piece;
+import de.nosswald.chess.game.piece.impl.Pawn;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
+/**
+ * @author Nils Osswald
+ * @author Noah Gerber
+ */
 public class Board
 {
-    private List<Piece> pieces;
+    private final List<Piece> pieces = new ArrayList<>();
+
+    public Board()
+    {
+        initialize();
+    }
+
+    public void initialize()
+    {
+        IntStream.range(0, 16).forEach(i -> {
+            boolean b = i % 2 == 0;
+            pieces.add(new Pawn(b ? Side.WHITE : Side.BLACK, b ? 1 : 7 , i / 2));
+        });
+
+        pieces.forEach(piece -> System.out.println(piece.getSide() + " " + piece.getCol() + " " + piece.getRow()));
+    }
+
+    public void paint(Graphics2D graphics, int boardSize)
+    {
+        // TODO paint board
+
+        // paint pieces
+        pieces.forEach(piece -> piece.paint(graphics, boardSize / 9));
+    }
 }
--- src/de/nosswald/chess/game/piece/Piece.java
+++ src/de/nosswald/chess/game/piece/Piece.java
@@ -1,4 +1,38 @@
 package de.nosswald.chess.game.piece;
 
-public class Piece {
+import de.nosswald.chess.game.Side;
+
+import java.awt.*;
+
+/**
+ * @author Nils Osswald
+ */
+public abstract class Piece
+{
+    private Side side;
+    private int col, row;
+
+    public Piece(Side side, int col, int row)
+    {
+        this.side = side;
+        this.col = col;
+        this.row = row;
+    }
+
+    public abstract void paint(Graphics2D graphics, int size);
+
+    public Side getSide()
+    {
+        return side;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
 }
