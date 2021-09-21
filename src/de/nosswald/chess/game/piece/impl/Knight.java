066c71d0fae79723aab81de63478fe94a0d6fa72
Noah.G <noahgerber100@gmail.com>
2021-09-21 12:33:01 +0200

    :sparkles: Added Pieces

---
+++ src/de/nosswald/chess/game/piece/impl/Knight.java
@@ -0,0 +1,20 @@
+package de.nosswald.chess.game.piece.impl;
+
+import de.nosswald.chess.game.Side;
+import de.nosswald.chess.game.piece.Piece;
+
+import java.awt.*;
+
+public class Knight extends Piece
+{
+    public Knight(Side side, int col, int row)
+    {
+        super(side, col, row);
+    }
+
+    @Override
+    public void paint(Graphics2D graphics, int size)
+    {
+        // TODO draw image
+    }
+}
