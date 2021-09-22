e26679e797e9059e759edaa16e31998cdbd63d42
Noah.G <noahgerber100@gmail.com>
2021-09-22 10:58:29 +0200

    :ambulance: Swaped colors

--- src/de/nosswald/chess/game/Board.java
+++ src/de/nosswald/chess/game/Board.java
@@ -25,17 +25,17 @@ public class Board
     {
         IntStream.range(0, 16).forEach(i -> {
             boolean b = i % 2 == 0;
-            pieces.add(new Pawn(b ? Side.WHITE : Side.BLACK, b ? 1 : 6, i / 2));
+            pieces.add(new Pawn(b ? Side.BLACK : Side.WHITE, b ? 1 : 6, i / 2));
             if (i < 4)
             {
-                pieces.add(new Rook(b ? Side.WHITE : Side.BLACK, b ? 0 : 7, i / 2 == 0 ? 0 : 7));
-                pieces.add(new Knight(b ? Side.WHITE : Side.BLACK, b ? 0 : 7, i / 2 == 0 ? 1 : 6));
-                pieces.add(new Bishop(b ? Side.WHITE : Side.BLACK, b ? 0 : 7, i / 2 == 0 ? 2 : 5));
+                pieces.add(new Rook(b ? Side.BLACK : Side.WHITE, b ? 0 : 7, i / 2 == 0 ? 0 : 7));
+                pieces.add(new Knight(b ? Side.BLACK : Side.WHITE, b ? 0 : 7, i / 2 == 0 ? 1 : 6));
+                pieces.add(new Bishop(b ? Side.BLACK : Side.WHITE, b ? 0 : 7, i / 2 == 0 ? 2 : 5));
             }
             if (i < 2)
             {
-                pieces.add(new Queen(b ? Side.WHITE : Side.BLACK, b ? 0 : 7,3));
-                pieces.add(new King(b ? Side.WHITE : Side.BLACK, b ? 0 : 7, 4));
+                pieces.add(new Queen(b ? Side.BLACK : Side.WHITE, b ? 0 : 7,3));
+                pieces.add(new King(b ? Side.BLACK : Side.WHITE, b ? 0 : 7, 4));
             }
         });
 
@@ -52,7 +52,7 @@ public class Board
         {
             for (int r = 0; r < 8; r++)
             {
-                graphics.setColor((c + r) % 2 == 1 ? new Color(240, 217, 181) : new Color(186, 120, 63));
+                graphics.setColor((c + r) % 2 == 1 ? new Color(186, 120, 63) : new Color(240, 217, 181));
                 graphics.fillRect((boardSize / 8) * r, (boardSize / 8) * c, boardSize / 8, boardSize / 8);
 
                 // paint piece
