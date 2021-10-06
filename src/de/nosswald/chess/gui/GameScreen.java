9b115df3a1f4a60418a1ee6a0975724ef846da84
Nils <imrebux@gmail.com>
2021-10-06 15:30:40 +0200

    :bulb: Optimized imports

--- src/de/nosswald/chess/gui/GameScreen.java
+++ src/de/nosswald/chess/gui/GameScreen.java
@@ -1,6 +1,5 @@
 package de.nosswald.chess.gui;
 
-import com.sun.xml.internal.ws.util.StringUtils;
 import de.nosswald.chess.Chess;
 import de.nosswald.chess.game.Side;
 
@@ -9,7 +8,6 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Arrays;
-import java.util.Locale;
 
 /**
  * @author Noah Gerber
